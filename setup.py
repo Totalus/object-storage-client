7a4fb0e24861125f575a84147bd49cd392d74eac
Totalus <34516402+Totalus@users.noreply.github.com>
2022-07-30 15:25:41 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,13 @@ from setuptools import setup
 import codecs
 import os.path
 
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-
+VERSION = '0.0.1'
+PACKAGE_NAME = 'universal-osc'
+AUTHOR = 'Totalus'
 
 setup(
-    version=get_version("src/__init__.py"),
-    name="gosc"
-    )
+    version=VERSION
+    name=PACKAGE_NAME,
+    author=AUTHOR,
+    package_dir={'gosc': 'src'}
+)
