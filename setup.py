631aa7d3e96c8e0de0df4135d3f8b2f26eef3852
Totalus <34516402+Totalus@users.noreply.github.com>
2022-07-30 15:26:36 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 
 from setuptools import setup
-import codecs
-import os.path
 
 VERSION = '0.0.1'
 PACKAGE_NAME = 'universal-osc'
 AUTHOR = 'Totalus'
 
 setup(
-    version=VERSION
+    version=VERSION,
     name=PACKAGE_NAME,
     author=AUTHOR,
     package_dir={'gosc': 'src'}
